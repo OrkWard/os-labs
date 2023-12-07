192d56767ac350292b1be1a02e169162340ae41e
OrkWard <orkward_o@outlook.com>
2023-12-08 01:18:05 +0800

    feat: finish lab1, part 1

--- lab1/arch/riscv/include/defs.h
+++ lab1/arch/riscv/include/defs.h
@@ -3,19 +3,19 @@
 
 #include "types.h"
 
-#define csr_read(csr)                       \
-({                                          \
-    register uint64 __v;                    \
-    /* unimplemented */                     \
-    __v;                                    \
-})
+#define csr_read(csr)                                                          \
+  ({                                                                           \
+    register uint64 __v;                                                       \
+    asm volatile("csrr t0, " #csr "\nmv %[value], t0"                          \
+                 : [value] "=r"(__v)                                           \
+                 :                                                             \
+                 : "memory", "t0") __v;                                        \
+  })
 
-#define csr_write(csr, val)                         \
-({                                                  \
-    uint64 __v = (uint64)(val);                     \
-    asm volatile ("csrw " #csr ", %0"               \
-                    : : "r" (__v)                   \
-                    : "memory");                    \
-})
+#define csr_write(csr, val)                                                    \
+  ({                                                                           \
+    uint64 __v = (uint64)(val);                                                \
+    asm volatile("csrw " #csr ", %0" : : "r"(__v) : "memory");                 \
+  })
 
 #endif
