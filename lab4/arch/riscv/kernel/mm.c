e2fb00819bfb5e23dd05da4af2407655c79bd744
OrkWard <orkward_o@outlook.com>
2023-12-25 05:47:41 +0800

    feat(lab4): copy from lab3

---
+++ lab4/arch/riscv/kernel/mm.c
@@ -0,0 +1,52 @@
+#include "mm.h"
+#include "defs.h"
+#include "string.h"
+
+#include "printk.h"
+
+extern char _ekernel[];
+
+struct {
+    struct run *freelist;
+} kmem;
+
+uint64 kalloc() {
+    struct run *r;
+
+    r = kmem.freelist;
+
+    kmem.freelist = r->next;
+
+    memset((void *)r, 0x0, PGSIZE);
+    return (uint64)r;
+}
+
+void kfree(uint64 addr) {
+    struct run *r;
+
+    // PGSIZE align
+    addr = addr & ~(PGSIZE - 1);
+
+    memset((void *)addr, 0x0, (uint64)PGSIZE);
+
+    r = (struct run *)addr;
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+
+    return;
+}
+
+void kfreerange(char *start, char *end) {
+    char *addr = (char *)PGROUNDUP((uint64)start);
+    int i = 0;
+    for (; (uint64)(addr) + PGSIZE <= (uint64)end; addr += PGSIZE) {
+        i += 1;
+        kfree((uint64)addr);
+    }
+    printk("We have free page of %d\n", i);
+}
+
+void mm_init(void) {
+    kfreerange(_ekernel, (char *)(VM_START + PHY_SIZE));
+    printk("...mm_init done!\n");
+}
