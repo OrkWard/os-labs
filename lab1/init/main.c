a0de03d213bc3acfc823c887db9bf5c59bc7a2ba
OrkWard <orkward_o@outlook.com>
2024-01-07 05:06:10 +0800

    docs(lab1): finish lab1 report

--- lab1/init/main.c
+++ lab1/init/main.c
@@ -1,3 +1,4 @@
+#include "defs.h"
 #include "printk.h"
 #include "sbi.h"
 
@@ -6,6 +7,13 @@ extern void test();
 int start_kernel() {
     printk("2023");
     printk(" Hello RISC-V\n");
+
+    uint64 sstatus = csr_read(sstatus);
+    printk("sstatus: %lx\n", sstatus);
+
+    csr_write(sscratch, 0x114514);
+    uint64 sscratch = csr_read(sscratch);
+    printk("sscratch: %lx\n", sscratch);
 
     test(); // DO NOT DELETE !!!
 
